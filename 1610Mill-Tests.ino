1a2141aad58b88dba832999061d95c798ce77765
John Glatts <johnglatts1@hotmail.com>
2019-06-05 12:58:47 -0400

    Update 1610Mill-Tests.ino

--- 1610Mill-Tests.ino
+++ 1610Mill-Tests.ino
@@ -11,32 +11,39 @@
  * NEG DIR = Away from Motor
  *
  * Need to calc. steps per inch
- *  - I.E -> how many steps do go one inch
  *  Current test
- *      - use the dirty way and try to record individual steps
  *      - First test:
  *          - 2653 steps = 1 inch
  *      - Second test:
  *          - 2540 steps = 1 inch --> this seems to be working good
  *
+ * 6/3/19 New Setup
+ *  - Use MM instead of inches
+ *  - Half to be careful when selecting new steps/rev speed, i.e half-step
+ *  - Have a macro setup just for MM's and pass in the length in MM
+ *      - 200 steps = 4mm
+ *      - 1mm = 50 steps
+ *
+ *  - The best results come from 1/16th stepping. The stepper moves slower,
+ *    but is able to reach the target more accurately
+ *
  */
 #include <AccelStepper.h>
 
 
-#define low_limit 11
-#define inch_limit 12
+// Pin defines
 #define STEP_PIN 9
 #define DIR_PIN 10
+#define low_limit 11
+#define inch_limit 12
 
 
-// macros for step indexingg
-#define INCH 2540
+// Macros for step indexing
+#define MM 800  // 50 * 16 --> motor is on 1/16 step
 
 
+// Setup a new instance of AccelStepper
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
-
-
-int increment = -1;
 
 
 void setup() {
@@ -47,54 +54,27 @@ void setup() {
     stepper.setMaxSpeed(1000);
     stepper.setSpeed(1000);
     stepper.setAcceleration(1000);
-    cutElement(1, .04); // testing a 1mm move, which is .04 inches  
+    homeMotor();
+    //cutElement(1, 4);  // 4mm move, measuring with calipers
     //homeMotor();
-}
-
-
-/* Pattern test */
-void testGroups() {
-    for (int i = 0; i < 3; ++i) {
-        stepper.moveTo(50000);
-        while (stepper.currentPosition() != 50000) // Full speed
-            stepper.run();
-        stepper.stop(); // Stop as fast as possible: sets new target
-        delay(500);
-        // blade will come down now
-        stepper.setCurrentPosition(0);  // reset the position and move on to another part
-    }
-}
-
-
-/* Another pattern test */
-void testPattern() {
-    for (int i = 0; i < 4; ++i) {
-        // testing precision movement
-        stepper.moveTo(-650);
-        while (stepper.currentPosition() != -650) // Full speed
-            stepper.run();
-        stepper.stop(); // Stop as fast as possible: sets new target
-        delay(250); // quarter second wait -- blade will come down
-        stepper.setCurrentPosition(0);  // reset the position and move on to another part
-    }
 }
 
 
 /* Home the motor, using limits, and reset the position */
 void homeMotor() {
-    int homing = 1;
+    int homing = -1;
 
     while (digitalRead(low_limit)) {
         stepper.moveTo(homing);
-        homing++;  // decrease by 1 for next move if needed
-        stepper.setSpeed(700);
+        homing--;  // decrease by 1 for next move if needed
+        stepper.setSpeed(5000);
         stepper.runSpeedToPosition(); // run the motor CCW towards the switch
     }
     stepper.setCurrentPosition(0);
 
     // move the stepper a bit away
-    stepper.moveTo(-100);
-    while (stepper.currentPosition() != -100) // Full speed
+    stepper.moveTo(700);
+    while (stepper.currentPosition() != 400) // Full speed
         stepper.run();
     stepper.stop();
     stepper.setCurrentPosition(0);
@@ -102,11 +82,11 @@ void homeMotor() {
 }
 
 
-/* Move the table in a series of 1 inch moves */
-void moveOneInch() {
+/* Move the table in a series of 1mm moves */
+void moveOneMM() {
     // testing precision movement
-    stepper.moveTo(-INCH);
-    while (stepper.currentPosition() != -INCH) // Full speed
+    stepper.moveTo(MM);
+    while (stepper.currentPosition() != MM) // Full speed
         stepper.run();
     stepper.stop(); // stop as fast as possible: sets new target
     delay(1000); // second wait -- blade will come down
@@ -115,14 +95,14 @@ void moveOneInch() {
 
 
 /* Used to calculate steps per inch */
-void calcInchMove() {
+void calcMove() {
     // set-up a limit switch 1" away
     // count steps and then further refine
-    int n_steps = -1;
+    int n_steps = 1;
 
     while (digitalRead(inch_limit))  {
         stepper.moveTo(n_steps);
-        n_steps--;  // decrease by 1 for next move if needed
+        n_steps++;  // decrease by 1 for next move if needed
         stepper.setSpeed(750);
         stepper.runSpeedToPosition(); // run the motor CCW towards the switch
         Serial.println(n_steps);    // debug the steps and find steps per inch
@@ -130,30 +110,32 @@ void calcInchMove() {
     stepper.stop();
 }
 
+
 /*
  * Cut the elements
  *
  *      @param quantity = number of elements to cut
- *      @param length = the length, in inches, of the G4 element
+ *      @param length = the length, in MM, of the G4 element
  *
  *
  */
 void cutElement(int quantity, float length) {
-    int cut_length = (int)INCH * length;
+    int cut_length = (int)MM * length;
 
     for (int i = 0; i < quantity; ++i) {
         // debug
         Serial.print("The amount of steps is : ");
         Serial.print(cut_length);
         Serial.println("");
-        stepper.moveTo(-cut_length);
-        while (stepper.currentPosition() != -cut_length) // Full speed
+        stepper.moveTo(cut_length);
+        while (stepper.currentPosition() != cut_length) // Full speed
             stepper.run();
         stepper.stop(); // stop as fast as possible: sets new target
-        delay(1000); // second wait -- blade will come down
+        delay(1000); // wait for one second -- blade will come down
         stepper.setCurrentPosition(0);
     }
 }
+
 
 void loop() {
 }
