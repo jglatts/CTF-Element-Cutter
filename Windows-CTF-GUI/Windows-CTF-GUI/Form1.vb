8735ba896af466a57957d4c90d8125b6ca6e7650
John Glatts <john@z-axiscc.com>
2019-06-13 17:03:46 -0400

    add new features

--- Windows-CTF-GUI/Windows-CTF-GUI/Form1.vb
+++ Windows-CTF-GUI/Windows-CTF-GUI/Form1.vb
@@ -3,12 +3,15 @@ Imports System.IO.Ports
 Imports System.Threading
 
 Public Class Form1
-
-    Shared _continue As Boolean
+    ' globals
+    Shared show_cut_length As Boolean
+    Shared are_traces_sent As Boolean
+    Shared is_quantity_sent As Boolean
+    Shared g_traces As Integer
     Shared SerialPort1 = New SerialPort()
-
+    Shared distance_travelled As Decimal
     Private Shared Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
+        ' set up serial port
         SerialPort1.PortName = "COM6" ' check And change Arduino port
         SerialPort1.BaudRate = 9600
         SerialPort1.DataBits = 8
@@ -17,10 +20,22 @@ Public Class Form1
         SerialPort1.Handshake = Handshake.None
         SerialPort1.Encoding = System.Text.Encoding.Default
 
+        ' hide cut length stuff
+        Form1.lblCutLength.Visible = False
+        Form1.txtCutLength.Visible = False
+        Form1.lblCutLengthInches.Visible = False
+        show_cut_length = False
+
+
     End Sub
     Private Sub btnHome_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHome.Click
 
         Dim b() As Byte = New Byte() {100}
+
+        distance_travelled = 0
+        txtDistTravel.Text = distance_travelled
+
+        btnDisableMotor.ForeColor = Color.Black
 
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
@@ -31,6 +46,8 @@ Public Class Form1
 
         Dim b() As Byte = New Byte() {101}
 
+        btnDisableMotor.ForeColor = Color.Black
+
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
         SerialPort1.Close()
@@ -39,6 +56,8 @@ Public Class Form1
     Private Sub btnCutElements_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCutElements.Click
 
         Dim b() As Byte = New Byte() {102}
+
+        btnDisableMotor.ForeColor = Color.Black
 
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
@@ -49,6 +68,8 @@ Public Class Form1
 
         Dim b() As Byte = New Byte() {103}
 
+        btnDisableMotor.ForeColor = Color.Red
+
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
         SerialPort1.Close()
@@ -57,6 +78,8 @@ Public Class Form1
     Private Sub btnBladeDown_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBladeDown.Click
 
         Dim b() As Byte = New Byte() {107}
+
+        btnDisableMotor.ForeColor = Color.Black
 
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
@@ -67,6 +90,8 @@ Public Class Form1
 
         Dim b() As Byte = New Byte() {106}
 
+        btnDisableMotor.ForeColor = Color.Black
+
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
         SerialPort1.Close()
@@ -75,7 +100,12 @@ Public Class Form1
     Private Sub btnSendTraces_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSendTraces.Click
         Dim value As Integer
         value = Convert.ToInt32(txtTraces.Text)
+        g_traces = value    ' set the global
         Dim b() As Byte = New Byte() {value}
+
+        btnDisableMotor.ForeColor = Color.Black
+        are_traces_sent = True  ' set the flag
+        checkWhatsBeenSent()    ' check the flags
 
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
@@ -90,6 +120,10 @@ Public Class Form1
         value += 200
         Dim b() As Byte = New Byte() {value}
 
+        btnDisableMotor.ForeColor = Color.Black
+        is_quantity_sent = True    ' set the flag
+        checkWhatsBeenSent()       ' check the flags
+
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
         SerialPort1.Close()
@@ -98,6 +132,11 @@ Public Class Form1
     Private Sub btnInchLeft_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnInchLeft.Click
         ' move one inch button
         Dim b() As Byte = New Byte() {113}
+
+        distance_travelled -= 1.0
+        txtDistTravel.Text = distance_travelled
+
+        btnDisableMotor.ForeColor = Color.Black
 
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
@@ -108,6 +147,11 @@ Public Class Form1
 
         Dim b() As Byte = New Byte() {114}
 
+        distance_travelled -= 0.1
+        txtDistTravel.Text = distance_travelled
+
+        btnDisableMotor.ForeColor = Color.Black
+
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
         SerialPort1.Close()
@@ -116,6 +160,11 @@ Public Class Form1
     Private Sub btnHunLeft_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHunLeft.Click
 
         Dim b() As Byte = New Byte() {115}
+
+        distance_travelled -= 0.01
+        txtDistTravel.Text = distance_travelled
+
+        btnDisableMotor.ForeColor = Color.Black
 
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
@@ -126,6 +175,11 @@ Public Class Form1
 
         Dim b() As Byte = New Byte() {116}
 
+        distance_travelled -= 0.001
+        txtDistTravel.Text = distance_travelled
+
+        btnDisableMotor.ForeColor = Color.Black
+
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
         SerialPort1.Close()
@@ -134,6 +188,12 @@ Public Class Form1
     Private Sub btnInchRight_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnInchRight.Click
         ' move one inch button
         Dim b() As Byte = New Byte() {109}
+
+        ' update the distance travelled textbox
+        distance_travelled += 1.0
+        txtDistTravel.Text = distance_travelled
+
+        btnDisableMotor.ForeColor = Color.Black
 
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
@@ -144,6 +204,11 @@ Public Class Form1
 
         Dim b() As Byte = New Byte() {110}
 
+        distance_travelled += 0.1
+        txtDistTravel.Text = distance_travelled
+
+        btnDisableMotor.ForeColor = Color.Black
+
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
         SerialPort1.Close()
@@ -152,6 +217,11 @@ Public Class Form1
     Private Sub btnHunRight_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHunRight.Click
 
         Dim b() As Byte = New Byte() {111}
+
+        distance_travelled += 0.01
+        txtDistTravel.Text = distance_travelled
+
+        btnDisableMotor.ForeColor = Color.Black
 
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
@@ -162,9 +232,28 @@ Public Class Form1
 
         Dim b() As Byte = New Byte() {112}
 
+        distance_travelled += 0.001
+        txtDistTravel.Text = distance_travelled
+
+        btnDisableMotor.ForeColor = Color.Black
+
         SerialPort1.Open()
         SerialPort1.Write(b, 0, 1)
         SerialPort1.Close()
 
+    End Sub
+    ' check the flags
+    Private Sub checkWhatsBeenSent()
+        Dim cut_length As Decimal
+        If are_traces_sent And is_quantity_sent Then
+            ' get cut length, in inches
+            cut_length = (g_traces + 1) / 2
+            cut_length = cut_length / 25.4
+            cut_length = Decimal.Round(cut_length, 3)
+            txtCutLength.Text = cut_length
+            lblCutLength.Visible = True
+            txtCutLength.Visible = True
+            lblCutLengthInches.Visible = True
+        End If
     End Sub
 End Class
