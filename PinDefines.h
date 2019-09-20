ed98e270a27319637f96b71450cf767797e59420
John Glatts <john@z-axiscc.com>
2019-09-20 17:03:47 -0400

    remove dead code

--- PinDefines.h
+++ PinDefines.h
@@ -22,20 +22,4 @@ double g4_cut_size;
 int g4_cut_quantity;
 
 
-/* 
-	Maybe constexpr will be better??
-#define RST 3
-#define RELAY_1 4
-#define RELAY_2 5
-#define ENA_PIN 6
-#define STOP_btn 7
-#define inc_btn 8
-#define STEP_PIN 9
-#define DIR_PIN 10
-#define low_limit 11
-#define inch_limit 12
-constexpr auto blade_down_delay 2400
-constexpr auto  blade_up_delay 2600
-#define MM 200  // 50 * 4 --> motor is on 1/4 step
 
-*/
